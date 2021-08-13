038b28227b150b41babc9271f1c0368e4e2986b0
ML <lawsonliu24@gmail.com>
2021-08-14 00:13:12 +1000

    FE add saga

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
 import roadmapSlice from '../components/roadmapPage/roadmapSlice/roadmapSlice';
+import { helloSaga } from './saga';
 
+const sagaMiddleware = createSagaMiddleware();
 export const store = configureStore({
   reducer: {
     roadmap: roadmapSlice,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(sagaMiddleware),
 });
-
+sagaMiddleware.run(helloSaga)
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 
